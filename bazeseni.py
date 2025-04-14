868883d0cc6f06de071eaef8ee78333d4d5d10f8
masuomehbahramiyan <masoomehbahramiyan77@gmail.com>
2025-04-14 12:26:51 -0500

    Add files via upload

---
+++ bazeseni.py
@@ -0,0 +1,11 @@
+age=int(input("Enter age:"))
+if 0<age and age<6:
+     print("khordsal")
+elif age==6 or age<10:
+     print("koodak")
+elif age==10 or age<24:
+     print("javan")
+elif age==24 or age<40:
+     print("bozorgsal")
+elif age==40 or age<40:
+    print("miyansal")
